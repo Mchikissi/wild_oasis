ee1cb557b17ce441975581e64ef302bf9606e685
Gift <ggmwanzia@gmail.com>
2024-06-28 17:29:32 +0300

    Fixed Navigation Menu Bug

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -72,7 +72,7 @@ export default function MainNav() {
         <li>
           <Link to='users'>
           <HiOutlineUsers/>
-          <span>Bookings</span>
+          <span>Users</span>
           </Link>
         </li>
         <li>
